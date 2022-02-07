551f5591164a0c420f4a8e5571a4de732e025af0
Steven Yung <stevenyung@fastmail.com>
2022-02-07 11:24:40 +0100

    prevent vite from replacing NODE_ENV
    https://github.com/vitejs/vite/issues/3229#issuecomment-1028093830

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,36 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    emptyOutDir: false,
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "RemixCrash",
-      fileName: (format) => `remix-crash.${format}.js`,
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react", "react-dom", "remix", "source-map", "fs/promises"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          remix: "Remix",
-          "source-map": "SourceMap",
-          "fs/promises": "FSPromises",
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    define:
+      mode === "production"
+        ? { "process.env.NODE_ENV": "process.env.NODE_ENV" }
+        : {},
+    build: {
+      emptyOutDir: false,
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "RemixCrash",
+        fileName: (format) => `remix-crash.${format}.js`,
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ["react", "react-dom", "remix", "source-map", "fs/promises"],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+            remix: "Remix",
+            "source-map": "SourceMap",
+            "fs/promises": "FSPromises",
+          },
         },
       },
     },
-  },
+  };
 });
